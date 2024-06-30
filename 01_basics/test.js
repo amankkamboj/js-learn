b17304acafbddddaf8f5bf931050fe41a351a79c
Aman <amankamboj2387@gmail.com>
2024-06-30 01:44:14 +0000

    Js execution cycle

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -59,6 +59,71 @@ function demoW(pram1,pram2){
  function calculateCartPrice2(val1,val2,...price){
         return price
  }
- console.log( calculateCartPrice(200,300,400) );
+//  console.log( calculateCartPrice(200,300,400) );
 
 
+// Scope in js
+
+// scope will be inside the curly braces for a and b variable but if using var to define the variable c it is applicable after the curly braces scope. If so many deveoplers working and they use the same name of variables with var key word that makes problem
+
+if(true){
+        let a = 10
+        const b = 20
+        var c = 30
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// This keyword used to repersent reference in current context
+
+const user = {
+        username:"Aman",
+        age:30,
+        welcomeMessage:function(){
+                console.log(`${this.username} , Welcome to user!!`);
+        }
+}
+// user.welcomeMessage();
+// user.username="Aman Kamboj"; //Changing current context
+// user.welcomeMessage();
+
+// const addTwo =(num1, num2)=>{
+//         return num1+num2;
+// }
+
+// const addTwo = (num1,num2) => (num1+num2)
+
+// Immediately Invoked Function Expressions (IIFE)
+
+(function AmanDev(){
+        console.log('DB CONNECTED');
+})(); //Always end IIFE
+// From global scope polution we IIFE
+
+( (uname) => {
+        console.log(`DB CONNECTED 2 ${uname}`)
+})("Aman");
+
+/*
+1.JS creates Global excution context 
+
+2.Next it creates memory phase
+
+3.Memory phase - In this phase,the variables are set to undefined 
+
+  until the execution phase(next phase) and the functions are set to their definitions.
+
+4.Next it creates the execution phase
+
+5.Execution phase - In this phase,the variables are initialsed to given values and 
+
+  when the function s are called,it creates a memory phase and execution phase for the function 
+
+  just like the main one.
+
+For live demo:
+browser > inspect > Sources
+create a snippet with few functions and visually experience the call stack.
+
+correct me if there are any mistakes or add to this if something is missing. */
